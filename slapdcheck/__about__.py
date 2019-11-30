216809ad78557b89e7e62586b3e35b21027cb97b
Michael Ströder <michael@stroeder.com>
2019-11-30 16:53:33 +0100

    set version to 3.1.0

--- slapdcheck/__about__.py
+++ slapdcheck/__about__.py
@@ -6,8 +6,8 @@ import collections
 
 VersionInfo = collections.namedtuple('version_info', ('major', 'minor', 'micro'))
 __version_info__ = VersionInfo(
-    major=4,
-    minor=0,
+    major=3,
+    minor=1,
     micro=0,
 )
 __version__ = '.'.join(str(val) for val in __version_info__)
