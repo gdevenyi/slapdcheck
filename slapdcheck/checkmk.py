df4377058073ae212692ef6f60d009fc71fe7123
Michael Ströder <michael@stroeder.com>
2019-12-01 20:09:17 +0100

    shortened slapdcheck.checkmk.__doc__

--- slapdcheck/checkmk.py
+++ slapdcheck/checkmk.py
@@ -1,24 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-monitoring check script for OpenLDAP
-
-Needs full read access to rootDSE and cn=config and cn=monitor
-(or whereever rootDSE attributes 'configContext' and 'monitorContext'
-are pointing to)
-
-Copyright 2015-2019 Michael Ströder <michael@stroeder.com>
-
-Licensed under the Apache License, Version 2.0 (the "License"); you may
-not use files and content provided on this web site except in compliance
-with the License. You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+slapd.checkmk - local check for check_mk
 """
 
 #-----------------------------------------------------------------------
