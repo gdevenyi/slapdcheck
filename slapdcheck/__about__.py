9f15e384125be82c115c63c4b698a69857c01d23
Michael Ströder <michael@stroeder.com>
2020-08-21 12:25:33 +0200

    bumped version to 3.6.9

--- slapdcheck/__about__.py
+++ slapdcheck/__about__.py
@@ -8,7 +8,7 @@ VersionInfo = collections.namedtuple('version_info', ('major', 'minor', 'micro')
 __version_info__ = VersionInfo(
     major=3,
     minor=6,
-    micro=8,
+    micro=9,
 )
 __version__ = '.'.join(str(val) for val in __version_info__)
 __author__ = u'Michael Stroeder'
