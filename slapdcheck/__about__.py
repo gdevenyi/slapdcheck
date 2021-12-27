e5445ce6afbdacf0a680c45caac5942308078104
Michael Ströder <michael@stroeder.com>
2021-12-27 23:43:00 +0100

    next release will be 2022

--- slapdcheck/__about__.py
+++ slapdcheck/__about__.py
@@ -13,7 +13,7 @@ __version_info__ = VersionInfo(
 __version__ = '.'.join(str(val) for val in __version_info__)
 __author__ = 'Michael Stroeder'
 __mail__ = 'michael@stroeder.com'
-__copyright__ = '(C) 2016-2021 by Michael Ströder <michael@stroeder.com>'
+__copyright__ = '(C) 2016-2022 by Michael Ströder <michael@stroeder.com>'
 __license__ = 'Apache-2.0'
 
 __all__ = [
